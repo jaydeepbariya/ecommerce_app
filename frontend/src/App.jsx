01df210909e6127b5114585632a7583f391b7685
jaydeepbariya <jaydeepbariya59@gmail.com>
2024-07-16 21:33:16 +0530

    folder structure, routes for auth module complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
-function App() {
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/common/Home.jsx"));
+const Login = lazy(() => import("./pages/Auth/Login.jsx"));
+const Register = lazy(() => import("./pages/Auth/Register.jsx"));
+const VerifyEmail = lazy(() => import("./pages/Auth/VerifyEmail.jsx"));
+const ForgotPassword = lazy(() => import("./pages/Auth/ForgotPassword.jsx"));
+const ChangePassword = lazy(() => import("./pages/Auth/ChangePassword.jsx"));
+const ResetPassword = lazy(() => import("./pages/Auth/ResetPassword.jsx"));
+const Loading = lazy(() => import("./pages/common/Loading.jsx"));
+
+export default function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/verify-email" element={<VerifyEmail />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/change-password" element={<ChangePassword />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 }
-
-export default App;
